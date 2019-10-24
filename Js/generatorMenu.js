4c7cdd91269499403a9d96b343cafbb567ec9afa
BlackFox3000 <flo-ti@hotmail.fr>
2019-10-24 16:59:59 +0200

    initialisation fichiers

---
+++ Js/generatorMenu.js
@@ -0,0 +1,19 @@
+function displayMenu(){
+    let menu='<div class="menu"> ' +
+        '<ul> ' +
+        '<li><a href="url.de.ma.page.html">item</a></li>' +
+        '<li><a href="url.de.ma.page.html">item</a></li>' +
+        '<li><a href="url.de.ma.page.html">item</a></li>' +
+        '<li><a href="url.de.ma.page.html">item</a></li>' +
+        '</ul> </div>';
+
+    document.write(menu)
+}
+
+function generatorMenu(tab) {
+    let menu='<div class="menu"><ul> ';
+    for(let i=0; i<tab.length ;i++){
+
+    }
+
+}
