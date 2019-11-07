0e67c676a245d1b2129bc7551b23f103351c901c
BlackFox3000 <flo-ti@hotmail.fr>
2019-11-07 13:14:15 +0100

    Generateur menu + fichier gulp manquant

--- Js/generatorMenu.js
+++ Js/generatorMenu.js
@@ -1,19 +1,35 @@
-function displayMenu(){
-    let menu='<div class="menu"> ' +
-        '<ul> ' +
-        '<li><a href="url.de.ma.page.html">item</a></li>' +
-        '<li><a href="url.de.ma.page.html">item</a></li>' +
-        '<li><a href="url.de.ma.page.html">item</a></li>' +
-        '<li><a href="url.de.ma.page.html">item</a></li>' +
-        '</ul> </div>';
+function displayMenu() {
+
+    console.log(arguments[0] instanceof Array);
+    let menu = '<div class="menu"> ' +
+        '<ul> ' + '<li><a href="eval_layout_menu.html">Accueil</a></li>';
+
+    if (arguments[0] instanceof Array) {
+        for (let i = 0; i < t.length; i++) {
+            menu += '<li><a href="Pages/' + t[i] + '.html">' + t[i] + '</a></li>';
+        }
+    } else {
+        for (let i = 0; i < arguments.length; i++) {
+            menu += '<li><a href="Pages/' + arguments[i] + '.html">' + arguments[i] + '</a></li>';
+        }
+    }
+    menu += '</ul> </div>';
 
     document.write(menu)
+
 }
 
-function generatorMenu(tab) {
-    let menu='<div class="menu"><ul> ';
-    for(let i=0; i<tab.length ;i++){
-
-    }
-
+function displayImage() {
+    console.log(document.title);
+    let title = document.title.charAt(0).toUpperCase() + document.title.substr(1).toLowerCase();
+    let image =
+        '<div class="content">' +
+        '<div class="inner" >' +
+        '<img class="image" src="../img/' + document.title + '.jpg">' +
+        '</div>' +
+        '<div class="titre">' +
+        title +
+        '</div>' +
+        '</div>';
+    document.write(image);
 }
